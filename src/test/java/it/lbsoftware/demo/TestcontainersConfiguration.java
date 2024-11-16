f74f1ea46b0863c8a96bfbfc2cb4ba49367edc7a
LBLucaBonetti <lb.luca.bonetti@gmail.com>
2024-11-16 15:05:26 +0100

    fix(spring-ai): add testcontainers configuration for ollama

--- src/test/java/it/lbsoftware/demo/TestcontainersConfiguration.java
+++ src/test/java/it/lbsoftware/demo/TestcontainersConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.ollama.OllamaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -13,5 +14,11 @@ class TestcontainersConfiguration {
   @ServiceConnection
   PostgreSQLContainer<?> postgresContainer() {
     return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+  }
+
+  @Bean
+  @ServiceConnection
+  OllamaContainer ollamaContainer() {
+    return new OllamaContainer(DockerImageName.parse("ollama/ollama:latest"));
   }
 }
